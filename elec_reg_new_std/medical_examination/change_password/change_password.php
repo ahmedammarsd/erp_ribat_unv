18af8e41c5dab282450d785e9d183a8e59acab62
ahmed amar <ahmed091999287@gmail.com>
2022-08-27 13:20:13 +0200

    check and update in medical examination and emp scintific affairs if the admin enter

--- elec_reg_new_std/medical_examination/change_password/change_password.php
+++ elec_reg_new_std/medical_examination/change_password/change_password.php
@@ -1,14 +1,10 @@
 <?php
 include "../../../connection/connection.php";
 session_start();
-
-
 $full_name = $_SESSION["full_name_scientific_affairs"];
 $display_data = mysqli_query($connection ,"select * from scientific_affairs_admins where full_name='$full_name'");
 $row = mysqli_fetch_array($display_data);
 $name_user =$row["full_name"];
-
-
 
 $display_password = mysqli_query($connection , "select password from scientific_affairs_admins where full_name='$full_name'");
 $row = mysqli_fetch_array($display_password);
@@ -38,6 +34,10 @@ if(isset($_POST["change"])){
     }
 }
 
+$name_user_admin = $_SESSION["full_name_scientific_affairs"] ;
+if ($name_user_admin != ""){
+    header("location: ../../admin/change_password/change_password.php");
+}
 ?>
 
 <!DOCTYPE html>
