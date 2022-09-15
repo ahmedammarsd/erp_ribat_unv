7daf60c99f30b40b42a73ed5b054fcea18e8c9b9
ahmed amar <ahmed091999287@gmail.com>
2022-09-15 10:00:58 +0200

    add main page and screenshoot

--- main/main.php
+++ main/main.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/all.min.css">
     <link rel="stylesheet" href="../bootstrap/bootstrap.min.css">
-    <link rel="stylesheet" href="../css/main/main.css?v=<?php echo time();?>">
+    <link rel="stylesheet" href="../css/main/main.css?v=<?php echo time(); ?>">
     <title>main</title>
 </head>
 <body> 
@@ -15,13 +15,30 @@
             <div class="container">
                 <h2>RIBAT UNVIRSITY</h2>
                 <ul>
-                    <li class="active"><a href ="#">Home</a></li>
+                    <li><a href ="#"  class="active">Home</a></li>
                     <li><a href ="..\elec_reg_new_std\form_number\form_number.php">ADDMISSION </a></li>
                     <li><a href ="..\student\login\login.php">LOGIN</a></li>
                 </ul>
             </div>
         </div>
     </div>
+    <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="../images/i.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="../images/i2.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="../images/i3.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="../images/i4.jpg" class="d-block w-100" alt="...">
+    </div>
+  </div>
 </div>
+<script src="../bootstrap/bootstrap.bundle.min.js"></script>
+<script src="../bootstrap/jquery.js"></script>
 </body>
 </html>
